d7ef74c70c26d256a1b23d30a0e148a9f5f2914b
VfdimMGIT <almaz_oven@mail.ru>
2025-02-16 23:12:36 +0500

    add cycle code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import pygame
-from pygame.examples.sprite_texture import running
+#from pygame.examples.sprite_texture import running
 
-pygame.init
+pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -20,9 +20,19 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-runnig = True
-while runnig:
-    pass
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+               target_x = random.randint(0, SCREEN_WIDTH - target_width)
+               target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+screen.blit(target_img, (target_x, target_y))
+pygame.display.update()
 
 pygame.quit()
